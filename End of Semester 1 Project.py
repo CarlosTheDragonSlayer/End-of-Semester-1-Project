e7b7493c47300e5d091293ca3d81011f33b91d37
Aubrey Nations <aubreynations@yahoo.com>
2014-12-03 14:56:57 -0600

    new files

--- End of Semester 1 Project.py
+++ End of Semester 1 Project.py
@@ -1,11 +1,11 @@
 from Tkinter import *
 root = Tk()
 
+dir1 = "C:\Users\Aubrey\Documents\GitHub\End of Semester 1 Project\End-of-Semester-1-Project\\"
+
 drawpad = Canvas(root, width=1360,height=700, background='#BFF5ED')
 drawpad.pack()
-from PIL import Image, ImageTk
-image = Image.open("Game BG.jpg")
-photo = ImageTk.PhotoImage(image)
-drawimg = drawpad.create_image(50,50,image=photo)
+gif1 = PhotoImage(file = 'C:\Users\Aubrey\Documents\GitHub\End of Semester 1 Project\End-of-Semester-1-Project\Player.gif')
+drawpad.create_image(700, 100, image = gif1)
 
 root.mainloop()
